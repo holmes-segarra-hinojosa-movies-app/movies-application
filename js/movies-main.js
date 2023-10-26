378950a81ff343292d94d810fbc5a676c6344fe2
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-10-26 14:28:39 -0500

    Working on adding js to movies-main and movies-api

--- js/movies-main.js
+++ js/movies-main.js
@@ -1,14 +1,60 @@
 "use strict";
-import * as movies from ".js/movies-api.js"
+// import * as movies from "./movies-api.js"
+import {getMovies, creatMovie, updateMovie, deleteMovie} from "./movies-api.js";
 
-function main() {
+let allMovies = [];
+async function main() {
+	allMovies = await getMovies();
+	console.log(allMovies);
+	for(let i=0; i < allMovies.length; i++){
+		let id = allMovies[i].id
+		let title = allMovies[i].title
+			let genre = allMovies[i].genre
+			let rating = allMovies[i].rating
+			let summary = allMovies[i].movieSummary
+			let poster = allMovies[i].posterURL
+		// console.log("ID: " +id)
+		// console.log("Title: " +title)
+		// console.log("Genre: " +genre)
+		// console.log("Rating: " +rating)
+		// console.log("Summary: " +summary)
+		// console.log("Poster: " +poster)
+	}
 
+	// let newMovie = await creatMovie({
+	// 	title: 'Fake Movie',
+	// 	genre: 'horror',
+	// 	rating: 2,
+	// 	movieSummary: 'Scary',
+	// 	posterURL: 'www.scary.com'
+	// })
 
+	// const updatedMovie = await updateMovie(newMovie.id, {
+	// 	title: newMovie.title,
+	// 	genre: newMovie.genre,
+	// 	rating: 3,
+	// 	movieSummary: 'Funny',
+	// 	posterURL: newMovie.posterURL
+	// })
 
 }
-
-
-
-
-
 main();
+
+// 'use strict'
+//
+// import {randGreet} from './greeting-logic.js'
+//
+// function main() {
+// 	document.querySelector('#greetButt').addEventListener('click', greetUser)
+// }
+//
+// function greetUser(){
+// 	let firstName = document.querySelector('#fName').value
+// 	console.log(firstName)
+//
+// 	let greetThem = randGreet() + " " + firstName;
+//
+// 	document.querySelector('.greeting').innerText =
+// 		greetThem;
+// }
+// main();
