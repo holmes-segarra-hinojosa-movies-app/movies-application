5598acd3bb0d8d754747bd7c10f3d69c9552f4cf
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-10-26 16:32:28 -0500

    Create movie working with html

--- js/movies-main.js
+++ js/movies-main.js
@@ -5,7 +5,7 @@ import {getMovies, creatMovie, updateMovie, deleteMovie} from "./movies-api.js";
 let allMovies = [];
 async function main() {
 	allMovies = await getMovies();
-	console.log(allMovies);
+	// console.log(allMovies);
 	for(let i=0; i < allMovies.length; i++){
 		let id = allMovies[i].id
 		let title = allMovies[i].title
@@ -14,37 +14,50 @@ async function main() {
 		let summary = allMovies[i].movieSummary
 		let poster = allMovies[i].posterURL
 		// console.log("ID: " +id)
-		console.log("Title: " +title)
+		// console.log("Title: " +title)
 		// console.log("Genre: " +genre)
 		// console.log("Rating: " +rating)
 		// console.log("Summary: " +summary)
 		// console.log("Poster: " +poster)
+		// console.log(allMovies);
 
-	let u = document.querySelector('.diver')
-		u.innerText = title;
-	// console.dir(u)
+	let showTitle = document.querySelector('.title').innerText = title;
+		// u.innerHTML = title;
+	console.log(showTitle)
 	}
 
 	//ADD MOVIE
-	// document.querySelector('#add-btn').addEventListener('click', createMovie)
-
-	// let x = document.querySelector('.add-btn')
-	// x.addEventListener('click', function(){
-	// 	// alert('click')
-	// 	let y = document.querySelector('.test').value
-	// 	console.log(y)
-	// })
-
-	// let newMovie = await creatMovie({
-	// 	title: 'Fake Movie',
-	// 	genre: 'horror',
-	// 	rating: 2,
-	// 	movieSummary: 'Scary',
-	// 	posterURL: 'www.scary.com'
-	// })
+	let addMovieBtn = document.querySelector('.add-btn')
+	addMovieBtn.addEventListener('click', async function(){
+		// alert('click')
+		let title = document.querySelector('#add-title').value
+		let genre = document.querySelector('#add-genre').value
+		let rating = document.querySelector('#add-rating').value
+		let movieSummary = document.querySelector('#add-summary').value
+		// let posetUrl = document.querySelector('#')
+		await creatMovie({
+			title: title,
+			genre: genre,
+			rating: rating,
+			movieSummary: movieSummary,
+			// posterURL:
+		})
+	})
 
 	///////////////EDIT MOVIE
-	// document.querySelector('#edit-btn').addEventListener('click', updateMovie)
+	let editMovieBtn = document.querySelector('.edit-btn')
+	editMovieBtn.addEventListener('click', async function(){
+
+		// await updateMovie(newMovie.id, {
+			// 	title: newMovie.title,
+			// 	genre: newMovie.genre,
+			// 	rating: 3,
+			// 	movieSummary: 'Funny',
+			// 	posterURL: newMovie.posterURL
+			// })
+
+	})
+		// .addEventListener('click', updateMovie)
 	// const updatedMovie = await updateMovie(newMovie.id, {
 	// 	title: newMovie.title,
 	// 	genre: newMovie.genre,
