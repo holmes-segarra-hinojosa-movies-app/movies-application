658fc8978b716256033ee9f915a67e2242d2568d
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-10-27 13:55:17 -0500

    Removed unused comments

--- js/movies-main.js
+++ js/movies-main.js
@@ -1,49 +1,23 @@
 "use strict";
-// import * as movies from "./movies-api.js"
+
 import {getMovies, creatMovie, updateMovie, deleteMovie} from "./movies-api.js";
 
 let allMovies = [];
-// let countCard = 1;
-// let countCardMade = 1;
-// let countTitle = 1;
-// let countGenre = 1;
-// let countRating = 1;
-// let countSummary = 1;
-// let newEditForCard = 1;
-// let btnEditer = 1;
-// let newDeleteForCard = 1;
-// let btnDeleter = 1;
-// let divEdit = 1;
+
 async function main() {
 	allMovies = await getMovies();
-	// console.log(allMovies);
+
+	let showLoad = document.querySelector('#loader')
+	showLoad.style.display = 'none';
 
 	for(let i=0; i < allMovies.length; i++){
-		// let mainElement = document.querySelector('main')
-		// let mainDiv = document.createElement('div')
-		// console.log(mainDiv[i])
-
-
-		// mainDiv[i].setAttribute('class', 'mainDiv')
-		// let c = mainElement[i].appendChild(mainDiv)
-		// console.log(mainElement[i])
 		let id = allMovies[i].id
 		let title = allMovies[i].title
 		let genre = allMovies[i].genre
 		let rating = allMovies[i].rating
 		let summary = allMovies[i].movieSummary
 		let poster = allMovies[i].posterURL
-		// console.log("ID: " +id)
-		// console.log("Title: " +title)
-		// console.log("Genre: " +genre)
-		// console.log("Rating: " +rating)
-		// console.log("Summary: " +summary)
-		// console.log("Poster: " +poster)
-		// console.log(allMovies);
-
 		let grabMain = document.querySelector('main')
-		// let grabCard = document.querySelector('.card')
-
 		let createDiv = document.createElement('div')
 		createDiv.classList.add('card', `card-${[i]}`)
 		grabMain.appendChild(createDiv)
@@ -59,10 +33,6 @@ async function main() {
 		showRating.setAttribute('class', `rating-${[i]}`)
 		let showSummary = document.createElement('p')
 		showRating.setAttribute('class', `summary-${[i]}`)
-		// let weatherIcon = document.createElement('img')
-		// weatherIcon.src = `https://openweathermap.org/img/w/${weatherId}.png`
-		// weatherIcon.setAttribute('id', "icon")
-
 
 
 		showTitle.innerText = title;
@@ -87,15 +57,10 @@ async function main() {
 		btnDivEdit.appendChild(makeEditBtn)
 
 
-		// let btnDelete = document.querySelector(`.newCards-${newDeleteForCard++}`)
 		let makeDelete = document.createElement('button')
 		makeDelete.classList.add('class',`delete-btn-${[i]}`)
 		makeDelete.innerText = 'Delete';
 		btnDivEdit.appendChild(makeDelete)
-
-
-
-
 
 	}
 
@@ -116,13 +81,12 @@ async function main() {
 		let genre = document.querySelector('#add-genre').value
 		let rating = document.querySelector('#add-rating').value
 		let movieSummary = document.querySelector('#add-summary').value
-		// let posetUrl = document.querySelector('#')
+
 		await creatMovie({
 			title: title,
 			genre: genre,
 			rating: rating,
 			movieSummary: movieSummary,
-			// posterURL:
 		})
 	})
 
@@ -147,47 +111,11 @@ async function main() {
 
 
 			let titleEdit = document.querySelector('#edit-title')
-			// let titleEd = await titleEdit.value = title;
 			let genreEdit = document.querySelector('#edit-genre')
-			// let genreEd = await genreEdit.value = genre;
 			let ratingEdit = document.querySelector('#edit-rating')
-			// let ratingEd = await ratingEdit.value = rating;
 			let summaryEdit = document.querySelector('#edit-summary')
-			// let summEd = await summaryEdit.value = summary
-
-
-
-			// let movie = await getMovies([i]);
-			// console.log(movie);
-
-			// make updateBook(book)
-			// need options for method, headers, and body
-			// test it
-			// book.title = "A Fake Book Part Deux";
-			// book = await updateBook(book);
-			//
-			// const changedBook = await getBook(5);
-			// console.log(book);
-
-
-			// await updateMovie(id.id, {
-			// 	title: titleEdit,
-			// 	genre: genreEdit,
-			// 	rating: ratingEdit,
-			// 	movieSummary: summaryEdit,
-			// 	// posterURL: editMovie.posterURL
-			// })
-			console.log(updateMovie)
 		})
 	}
-		// .addEventListener('click', updateMovie)
-	// const updatedMovie = await updateMovie(newMovie.id, {
-	// 	title: newMovie.title,
-	// 	genre: newMovie.genre,
-	// 	rating: 3,
-	// 	movieSummary: 'Funny',
-	// 	posterURL: newMovie.posterURL
-	// })
 
 	for(let i=0; i < allMovies.length; i++) {
 		let deleteMovieButton = document.querySelector(`.delete-btn-${[i]}`)
@@ -204,21 +132,3 @@ async function main() {
 
 main();
 
-// 'use strict'
-//
-// import {randGreet} from './greeting-logic.js'
-//
-// function main() {
-// 	document.querySelector('#greetButt').addEventListener('click', greetUser)
-// }
-//
-// function greetUser(){
-// 	let firstName = document.querySelector('#fName').value
-// 	console.log(firstName)
-//
-// 	let greetThem = randGreet() + " " + firstName;
-//
-// 	document.querySelector('.greeting').innerText =
-// 		greetThem;
-// }
-// main();
